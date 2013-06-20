179b9de0f90ffc08c87a5dff5a6ce3806e16ce5e
Justin Klemm <jwklemm@gmail.com>
2013-06-20 12:09:07 -0400

    Tweaks to initial data setup

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(function () {
+  
+  // clear all existing spots
+  //Spots.remove({});
+
   if (Spots.find().count() === 0) {
     var data = [
       { name: "Sushi Yasuda",
-        description: "A brief description of the restaurant goes here.",
+        description: "204 East 43rd St., New York, NY 10017",
         votes: 0,
         timestamp: (new Date()).getTime()
       },
       { name: "Ushiwakamaru",
-        description: "A brief description of the restaurant goes here.",
+        description: "136 W Houston St., New York, NY 10012",
         votes: 0,
         timestamp: (new Date()).getTime()
       },
       { name: "Sushi Azabu",
-        description: "A brief description of the restaurant goes here.",
+        description: "428 Greenwich St., New York, NY 10013",
         votes: 0,
         timestamp: (new Date()).getTime()
       }
